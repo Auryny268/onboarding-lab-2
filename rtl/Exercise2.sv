65162a341592bd05a15e37e57d6bb073b0074f55
aay268 <aay268@nyu.edu>
2024-02-12 18:22:19 -0500

    (fix): Finished all exercises

--- rtl/Exercise2.sv
+++ rtl/Exercise2.sv
@@ -17,10 +17,12 @@ module Exercise2 (
 // wire xor2 = out[12] ^ xor1;
 // wire xor3 = out[10] ^ xor2;
 
+wire [15:0] taps = {15'b0, (out[10] ^ (out[12] ^ (out[13] ^ out[15])))};
+
 always @(posedge clk)
   if (!nReset)
     out <= init;
   else
-    out <= (out << 1) | (out[10] ^ (out[12] ^ (out[13] ^ out[15])));
+    out <= (out << 1) | taps;
 
 endmodule
